6474dac109bf5cd24da706b1161d1d36ab5b138b
Kailash Choudhary <kailash_choudhary@srmap.edu.in>
2023-10-25 12:45:52 +0530

    ik im mad but i had to do it all over again

--- app/page.js
+++
@@ -1,9 +0,0 @@
-import Navbar from "./components/Navbar";
-import Content from "./components/Content";
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-4 font-main bg-main relative">
-      <Navbar />
-    </main>
-  );
-}
